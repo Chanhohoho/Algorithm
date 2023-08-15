a8231a30535a24cb4b9133fe087c803ea4482ae6
박찬호 <cksgh0109@naver.com>
2023-08-15 22:09:40 +0900

    JadenCase 문자열 만들기

---
+++ JadenCase 문자열 만들기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    int i = 0;
+    bool check = true;
+    
+    while(i<s.size()){
+    
+        if(s[i]==' ') {
+            check=true;
+        }
+        else if(check){
+            if(s[i]>='a' && s[i]<='z') s[i]+='A'-'a';
+            check=false;
+        }
+        else{
+            if(s[i]>='A' && s[i]<='Z') s[i]+='a'-'A';
+        }
+        
+        i++;
+    }
+    
+    answer = s;
+    
+    return answer;
+}
