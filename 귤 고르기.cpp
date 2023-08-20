69e13f4b0a3334f4172ffb49ce92937e5371812b
박찬호 <cksgh0109@naver.com>
2023-08-20 23:10:07 +0900

    귤 고르기

---
+++ 귤 고르기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(int front, int back){
+    return front>back;
+}
+
+int solution(int k, vector<int> tangerine) {
+    
+    int answer = 0;
+    
+    vector<int> DAT(10000001, 0);
+    
+    for(int i=0; i<tangerine.size(); i++)   DAT[tangerine[i]]++;
+    
+    sort(DAT.begin(), DAT.end(), compare);
+        
+    while(k>0)  k-=DAT[answer++];
+        
+    return answer;
+}
