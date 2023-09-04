115ca65045bc6039b62de9a6ed733f5e37677c96
박찬호 <cksgh0109@naver.com>
2023-09-04 13:19:41 +0900

    최댓값과 최솟값

---
+++ 최댓값과 최솟값.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    int i = 0;
+    string temp = "";
+    
+    int maxN = -123456789;
+    int minN = 123456789;
+    
+    while(i<s.size()){
+        
+        if(s[i]==' '){
+            int now = stoi(temp);
+            if(maxN<now) maxN=now;
+            if(minN>now) minN=now;
+            temp = "";
+        }
+        else    temp+=s[i];
+        
+        i++;
+        
+    }
+    
+    int now = stoi(temp);
+    if(maxN<now) maxN=now;
+    if(minN>now) minN=now;
+
+    answer = to_string(minN) + ' ' + to_string(maxN);
+    
+    return answer;
+}
