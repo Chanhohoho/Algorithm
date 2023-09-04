744b0cff0f92be6fbdb81b34ddc87fadc1935ec8
박찬호 <cksgh0109@naver.com>
2023-09-04 15:07:16 +0900

    카펫

---
+++ 프로그래머스/Level 2/카펫.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    
+    for(int i = 1; i<=sqrt(yellow); i++){   
+        // i - 노란색 카펫 세로길이
+        // j - 노란색 카펫 가로길이
+        if(yellow%i!=0) continue;
+        int j = yellow/i;
+        int brownNeed = (i+j)*2+4;
+        
+        if(brown == brownNeed){
+            answer.push_back(j+2);
+            answer.push_back(i+2);
+            break;
+        }
+        
+    }
+    
+    return answer;
+}
