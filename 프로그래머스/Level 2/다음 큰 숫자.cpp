dfe5e229c5817c9fa7aa74cc3b244a9f0940b884
박찬호 <cksgh0109@naver.com>
2023-09-04 14:27:58 +0900

    다음 큰 숫자

---
+++ 프로그래머스/Level 2/다음 큰 숫자.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    int cnt1=0;
+    for(int i=n; i>0; i/=2) if(i%2==1) cnt1++;
+    
+    int now = n+1;
+    int check = -1;
+    
+    while(cnt1!=check){
+        
+        check=0;
+        for(int i=now; i>0; i/=2) if(i%2==1) check++;
+        now++;
+        
+    }
+    
+    answer = now-1;
+    
+    return answer;
+}
