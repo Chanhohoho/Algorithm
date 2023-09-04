8635975bbd4d81cc74b315cf2e9a2ec14bc283e6
박찬호 <cksgh0109@naver.com>
2023-09-04 15:17:14 +0900

    영어 끝말잇기

---
+++ 프로그래머스/Level 2/영어 끝말잇기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    
+    vector<int> answer;
+    int i;
+    string all = " ";
+    
+    for (i = 1; i < words.size(); i++) {
+        all+=words[i-1]+" ";
+        if (words[i - 1][words[i - 1].size() - 1] != words[i][0]) break;
+        if(all.find(' ' + words[i] + ' ')!=string::npos) break;
+    }
+    
+    if (i == words.size()) {
+        answer.push_back(0);
+        answer.push_back(0);
+    }
+    else {
+        answer.push_back(i%n+1);
+        answer.push_back(i/n+1);
+    }
+    
+    return answer;
+}
