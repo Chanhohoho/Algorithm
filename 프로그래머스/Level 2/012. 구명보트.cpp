5c28c0897ad5c45e2229201c2189c6d959c0a07b
박찬호 <cksgh0109@naver.com>
2023-09-08 17:19:52 +0900

    구명보트

--- 프로그래머스/Level 2/012. 구명보트.cpp
+++ 프로그래머스/Level 2/012. 구명보트.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(vector<int> people, int limit) {
     int answer = 0;
     
-    sort(people.rbegin(), people.rend());
+    sort(people.rbegin(), people.rend());   // 내림차순으로 정렬
     
     for(int i=0, j=people.size()-1; i<=j; i++){
         if(people[i] + people[j] <= limit) j--;
