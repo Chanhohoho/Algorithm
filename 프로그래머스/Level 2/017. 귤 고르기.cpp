9dcecece49ee6c26d5256383c3a60c021247e4d7
박찬호 <cksgh0109@naver.com>
2023-09-08 18:54:13 +0900

    귤 고르기

--- 프로그래머스/Level 2/017. 귤 고르기.cpp
+++ 프로그래머스/Level 2/017. 귤 고르기.cpp
@@ -8,10 +8,9 @@ bool compare(int front, int back){
 }
 
 int solution(int k, vector<int> tangerine) {
-    
     int answer = 0;
     
-    vector<int> DAT(10000001, 0);
+    vector<int> DAT(10000001, 0);   // 메모리 할당
     
     for(int i=0; i<tangerine.size(); i++)   DAT[tangerine[i]]++;
     
