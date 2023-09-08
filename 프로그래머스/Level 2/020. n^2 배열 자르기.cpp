958c803fce3745791a1a6a12b187b6646b1b433a
박찬호 <cksgh0109@naver.com>
2023-09-08 19:16:38 +0900

    n^2 배열 자르기

--- 프로그래머스/Level 2/020. n^2 배열 자르기.cpp
+++ 프로그래머스/Level 2/020. n^2 배열 자르기.cpp
@@ -7,8 +7,8 @@ vector<int> solution(int n, long long left, long long right) {
     
     for(; left<=right; left++){
         
-        int i = left/n;
-        int j = left%n;
+        int i = left/n; // 행
+        int j = left%n; // 열
 
         if(i>=j) answer.push_back(i+1);
         else     answer.push_back(j+1);
