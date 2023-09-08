cbf0f310c53238fffcfd80348b657cafebb4558c
박찬호 <cksgh0109@naver.com>
2023-09-08 17:04:55 +0900

    다음 큰 숫자

--- 프로그래머스/Level 2/007. 다음 큰 숫자.cpp
+++ 프로그래머스/Level 2/007. 다음 큰 숫자.cpp
@@ -9,7 +9,7 @@ int solution(int n) {
     for(int i=n; i>0; i/=2) if(i%2==1) cnt1++;
     
     int now = n+1;
-    int check = -1;
+    int check = -1; // 1의 갯수
     
     while(cnt1!=check){
         
